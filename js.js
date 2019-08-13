98d485b3835f96dcfcc518c4fe2225e5342ff8f9
md-one <mehdi.black555@gmail.com>
2019-08-13 23:30:01 +0100

    adding phone version
    + adjusting the animation

--- js.js
+++ js.js
@@ -1,64 +1,123 @@
-const img = document.querySelectorAll("#shop img");
-const overLay = document.querySelectorAll("#shop .overLay");
-
-let i;
-
-for (let i = 0; i < img.length; i++) {
-    img[i].addEventListener("mouseenter", name())
-
-
-    function name() {
-        img[i].onmouseover = function () {
-            mouseOver()
-        };
-        img[i].onmouseout = function () {
-            mouseOut();
-        }
-    };
-
-    function mouseOver() {
-        overLay[i].style.display = "block";
-    }
-
-    function mouseOut() {
-        overLay[i].style.display = "none";
-    }
-}
-
-for (let i = 0; i < img.length; i++) {
-
-    img[i].onmouseover = function () {
-        overLay[i].style.display = "block";
-    };
-
-    img[i].onmouseout = function () {
-        overLay[i].style.display = "none";
-    };
-
-}
-
-
 var rightImg = document.querySelectorAll(".rightImg");
 var leftImg = document.querySelectorAll(".leftImg");
 
 
 var controller = new ScrollMagic.Controller({
     globalSceneOptions: {
-        duration: 100
+        //duration: 500
     }
 });
 
-var scene = new ScrollMagic.Scene({
-    triggerElement: "#imgGrid",
-    duration: 100
-}).on("enter", function () {
-    TweenMax.to(rightImg, 1, {
-        x: "100%",
-        transformOrigin: "0% 100%"
-    });
+// i make 3 scene for every row in the grid because of the different size of images and i made the triggerElement the bigger element in the row
 
-}).on("leave", function () {
-    TweenMax.to(rightImg, 1, {
-        x: "0%"
-    })
-}).addTo(controller);
+let q = window.matchMedia("(max-width: 768px)");
+
+if (q.matches) {
+    for (let i = 0; i < rightImg.length; i++) {
+
+        var scene = new ScrollMagic.Scene({
+            triggerElement: leftImg[0]
+        }).on("enter", function () {
+            TweenMax.to(rightImg[i], 1, {
+                opacity: 1
+            });
+
+            TweenMax.to(leftImg[i], 1, {
+                opacity: 1
+            });
+
+        }).on("leave", function () {
+            TweenMax.to(rightImg[i], 1, {
+                opacity: 0
+            })
+            TweenMax.to(leftImg[i], 1, {
+                opacity: 0
+            });
+        }).addTo(controller);
+
+    }
+
+
+} else {
+    var scene = new ScrollMagic.Scene({
+        triggerElement: leftImg[0]
+    }).on("enter", function () {
+        TweenMax.to(rightImg[0], 1, {
+            x: "-50%",
+            transformOrigin: "0% 100%",
+            opacity: 1
+
+        });
+
+        TweenMax.to(leftImg[0], 1, {
+            x: "50%",
+            transformOrigin: "0% 100%",
+            opacity: 1
+        });
+
+    }).on("leave", function () {
+        TweenMax.to(rightImg[0], 1, {
+            x: "0%",
+            opacity: 0
+        })
+        TweenMax.to(leftImg[0], 1, {
+            x: "0%",
+            transformOrigin: "0% 100%",
+            opacity: 0
+        });
+    }).addTo(controller);
+
+    var scene = new ScrollMagic.Scene({
+        triggerElement: rightImg[1]
+    }).on("enter", function () {
+        TweenMax.to(rightImg[1], 1, {
+            x: "-50%",
+            transformOrigin: "0% 100%",
+            opacity: 1
+        });
+
+        TweenMax.to(leftImg[1], 1, {
+            x: "50%",
+            transformOrigin: "0% 100%",
+            opacity: 1
+        });
+
+    }).on("leave", function () {
+        TweenMax.to(rightImg[1], 1, {
+            x: "0%",
+            opacity: 0
+        })
+        TweenMax.to(leftImg[1], 1, {
+            x: "0%",
+            transformOrigin: "0% 100%",
+            opacity: 0
+        });
+    }).addTo(controller);
+
+    var scene = new ScrollMagic.Scene({
+        triggerElement: leftImg[2]
+    }).on("enter", function () {
+        TweenMax.to(rightImg[2], 1, {
+            x: "-50%",
+            transformOrigin: "0% 100%",
+            opacity: 1
+        });
+
+        TweenMax.to(leftImg[2], 1, {
+            x: "50%",
+            transformOrigin: "0% 100%",
+            opacity: 1
+        });
+
+    }).on("leave", function () {
+        TweenMax.to(rightImg[2], 1, {
+            x: "0%",
+            opacity: 0
+        })
+        TweenMax.to(leftImg[2], 1, {
+            x: "0%",
+            transformOrigin: "0% 100%",
+            opacity: 0
+        });
+    }).addTo(controller);
+}
